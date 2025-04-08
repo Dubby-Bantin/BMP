175e6b57a18ee5b1136de1c5ba86815be290c932
Dubby-Bantin <dubbybantin@icloud.com>
2025-04-08 18:22:05 -0400

    made updates

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
+import Link from "next/link";
 
 interface AuthorBannerProps {
   name: string;
@@ -11,12 +12,12 @@ const AuthorBanner: React.FC<AuthorBannerProps> = ({ name, bio, imageUrl }) => {
   return (
     <div className="relative rounded-xl overflow-hidden flex items-center justify-center w-full">
       <div className="md:flex items-center justify-between w-full gap-55">
-        <div className="md:w-1/2 text-center pl-50">
-          <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-2">
+        <div className="md:w-1/2 text-center pl-20">
+          <h1 className="text-3xl md:text-5xl font-bold text-gray-800 mb-2">
             {name}
           </h1>
 
-          <p className="text-gray-700 mb-4">{bio}</p>
+          <p className="text-gray-700 mb-4 md:text-xl">{bio}</p>
         </div>
 
         <div className="md:w-[750px] relative h-[750px]">
@@ -28,15 +29,18 @@ const AuthorBanner: React.FC<AuthorBannerProps> = ({ name, bio, imageUrl }) => {
           />
         </div>
       </div>
-      <div className="shadow-md absolute rounded-md h-[18rem] w-[18rem] bg-white right-[34rem] top-32 flex flex-col items-center justify-between p-5">
+      <div className="shadow-md absolute rounded-md h-[18rem] w-[18rem] bg-white right-[34rem] top flex flex-col items-center justify-between p-5">
         <h1 className="text-[#1da0f1] font-bold text-2xl">
           Be the first to know!{" "}
         </h1>
         <p className="">
-          Get notified when <span className="italic font-bold">MONEY DISORDER</span> and other book updates
-          are live. Join the waitlist{" "}
+          Get notified when{" "}
+          <span className="italic font-bold">MONEY DISORDER</span> and other
+          book updates are live. Join the waitlist{" "}
         </p>
-        <Button variant={"outline"}>Join the waitlist!</Button>
+        <Button className="bg-black text-white" asChild variant={"outline"}>
+          <Link href={"#contact"}>Join the waitlist!</Link>
+        </Button>
       </div>
     </div>
   );
