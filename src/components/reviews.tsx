f00f80d4214cab99578eae9efb662fcecb054ce9
Dubby-Bantin <dubbybantin@icloud.com>
2025-04-14 12:51:32 -0400

    changed banner text

--- src/components/reviews.tsx
+++ src/components/reviews.tsx
@@ -6,23 +6,31 @@ const ReviewsSection = () => {
         id="reviews"
         className="py-20 bg-[#1da0f1] flex items-center justify-between text-white  p-4 text-center gap-20 md:w-[70rem]"
       >
-        <p>
-          A personable look at a fascinating life. The author lets us in to his
-          unique journey from childhood to parenthood, from student to
-          professional, from his native land to several places outside his
-          country. It is motivational with many examples of overcoming
-          challenges. If you&apos;re looking for an easy read with rich cultural
-          references to African history, specifically Nigeria, then this book is
-          for you. Uukwu, Amazon verified purchase.
-        </p>
-        <p>
-          Thank you for the book &quot;Young Man In A Hurry&quot; I highly
-          recommend it. Patience, perseverance, determination, persistence and
-          preparation makes an unbeatable combination for Success. The
-          remarkable thing we have is a choice every day regarding the attitude
-          we will embrace for the day or how we choose to show up for our self.
-          I salute you. EkeleG, Amazon verified purchase
-        </p>
+        <div className="flex flex-col items-center gap-5">
+          <p>
+            A personable look at a fascinating life. The author lets us in to
+            his unique journey from childhood to parenthood, from student to
+            professional, from his native land to several places outside his
+            country. It is motivational with many examples of overcoming
+            challenges. If you&apos;re looking for an easy read with rich
+            cultural references to African history, specifically Nigeria, then
+            this book is for you. , Amazon verified purchase.
+          </p>
+          <small>-Uukwu</small>
+        </div>
+
+        <div className="flex flex-col items-center gap-5">
+          {" "}
+          <p>
+            Thank you for the book &quot;Young Man In A Hurry&quot; I highly
+            recommend it. Patience, perseverance, determination, persistence and
+            preparation makes an unbeatable combination for Success. The
+            remarkable thing we have is a choice every day regarding the
+            attitude we will embrace for the day or how we choose to show up for
+            our self. I salute you. , Amazon verified purchase
+          </p>
+          <small>-EkeleG</small>
+        </div>
       </div>
     </section>
   );
